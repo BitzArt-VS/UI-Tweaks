5f4356719bee70222e42cc5f1026b470810cef39
Yuriy Durov <yu.durov@gmail.com>
2026-03-09 20:34:31 +0400

    Project structure & release workflow

--- src/QuickSearch/Common/ModHotKeys.cs
+++ src/QuickSearch/Common/ModHotKeys.cs
@@ -6,25 +6,28 @@ namespace BitzArt.QuickSearch;
 
 public static class ModHotKeys
 {
-    public static ModHotKey QuickSearch { get; } = ModHotKey.FromLang(Constants.ModId, "search", "hotKey_name", GlKeys.N);
+    public static ModHotKey QuickSearch { get; }
+        = ModHotKey.FromLang(Constants.ModId, "search", HotkeyType.HelpAndOverlays, "quickSearchHotKeyName", GlKeys.N);
 }
 
 public record ModHotKey
 {
     public string Code { get; private init; }
+    public HotkeyType HotkeyType { get; private init; }
     public string? Name { get; private init; }
     public GlKeys? DefaultKey { get; private init; }
 
-    public ModHotKey(string code, string? name = null, GlKeys? defaultKey = null)
+    public ModHotKey(string code, HotkeyType type, string? name = null, GlKeys? defaultKey = null)
     {
         Code = code;
+        HotkeyType = type;
         Name = name;
         DefaultKey = defaultKey;
     }
 
-    public static ModHotKey FromLang(string modId, string code, string? name, GlKeys? defaultKey = null)
+    public static ModHotKey FromLang(string modId, string code, HotkeyType type, string? name, GlKeys? defaultKey = null)
     {
-        return new ModHotKey($"{modId}:{code}", Lang.Get($"{modId}:{name}"), defaultKey);
+        return new ModHotKey($"{modId}:{code}", type, Lang.Get($"{modId}:{name}"), defaultKey);
     }
 }
 
