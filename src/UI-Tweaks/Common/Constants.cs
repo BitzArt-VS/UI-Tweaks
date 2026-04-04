4487e1de0731d1fcaf1d55898ecb0dc533e0c919
Yuriy Durov <yu.durov@gmail.com>
2026-04-04 09:30:44 +0400

    Fix config file name

--- src/UI-Tweaks/Common/Constants.cs
+++ src/UI-Tweaks/Common/Constants.cs
@@ -4,5 +4,5 @@ internal static class Constants
 {
     public const string ModName = "UI-Tweaks";
     public const string ModId = "uitweaks";
-    public const string ModConfigFileName = $"{ModId}.json";
+    public const string ModConfigFileName = $"ui-tweaks.json";
 }
